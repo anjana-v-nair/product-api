d881a19e46dfcf74a9c16f6e4d042f53fddad29e
Anjana Nair <anjuviswan93@gmail.com>
2023-04-20 11:47:04 +0530

    Update Item.java

--- src/main/java/com/example/product/domain/Item.java
+++ src/main/java/com/example/product/domain/Item.java
@@ -1,12 +1,11 @@
 package com.example.product.domain;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "item")
