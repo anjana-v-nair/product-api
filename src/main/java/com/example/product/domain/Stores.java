ce97af96d1b60fd6e30edbdec2cb6af7fdaa3c2d
Anjana Nair <anjuviswan93@gmail.com>
2023-04-20 11:47:35 +0530

    Update Stores.java

--- src/main/java/com/example/product/domain/Stores.java
+++ src/main/java/com/example/product/domain/Stores.java
@@ -1,11 +1,11 @@
 package com.example.product.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
