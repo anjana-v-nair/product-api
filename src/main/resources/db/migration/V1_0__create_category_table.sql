a51e2d27b826aba2543ac21d6632056985e24fc6
Anjana Nair <anjuviswan93@gmail.com>
2023-04-20 08:05:48 +0530

    added flyway scripts

---
+++ src/main/resources/db/migration/V1_0__create_category_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE category
+(
+    category_id BIGINT NOT NULL,
+    category_name VARCHAR(500) NOT NULL,
+    category_description VARCHAR(500) NOT NULL,
+    CONSTRAINT category_id_pkey PRIMARY KEY (category_id)
+)
---
+++ src/main/resources/db/migration/V1_4__create_stores_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE stores
+(
+    stores_id BIGINT NOT NULL,
+    stores_location VARCHAR(500) NOT NULL,
+    CONSTRAINT stores_id_pkey PRIMARY KEY (stores_id)
+)
