f598d7805d495e8bfbbc358c2976d207276ba846
Anjana Nair <anjuviswan93@gmail.com>
2023-04-20 11:22:27 +0530

    Update Category.java

--- src/main/java/com/example/product/domain/Category.java
+++ src/main/java/com/example/product/domain/Category.java
@@ -14,4 +14,15 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @Builder
 public class Category {
+  
+  @Id
+  private Long categoryId;
+  private String categoryName;
+  private String  categoryDescription;
+  
+  @OneToMany
+  @JoinColumn(name = "item_category")
+  private List<Item> items;
+  
+}
 }
